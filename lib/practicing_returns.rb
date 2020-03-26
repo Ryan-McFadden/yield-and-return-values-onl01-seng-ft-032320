95523a16efaf11fb47f4c74d99249904b8cbc84a
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-03-26 08:07:09 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -3,10 +3,11 @@ require 'pry'
 def hello(array)
   i = 0
   collection = []
-  while i < array.count
+  while i < array.length
     collection << yield(array[i])
     i += 1
   end
+  collection
 end
 
 hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
